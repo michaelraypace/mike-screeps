501931ce0a517b791aabec709bfe330681779a41
michaelraypace <michael.ray.pace@gmail.com>
2017-03-05 21:33:51 -0600

    find dropped energy

--- creep.actions.js
+++ creep.actions.js
@@ -105,10 +105,18 @@ actions.general.doWork = function(creep) {
 }
 
 actions.general.harvestClosestResource = function(creep) {
-
+  
   const sources = creep.room.find(FIND_SOURCES);
   const closestSource = creep.pos.findClosestByPath(sources);
-  if (creep.harvest(closestSource) === ERR_NOT_IN_RANGE) {
+  const energy = creep.pos.findInRange(FIND_DROPPED_ENERGY, 5);
+  if (energy.length) {
+        console.log('found ' + energy[0].energy + ' energy at ', energy[0].pos);
+        if(creep.pickup(energy[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(energy[0]);
+        }
+  }
+
+  else if (creep.harvest(closestSource) === ERR_NOT_IN_RANGE) {
     creep.moveTo(closestSource, {visualizePathStyle: {stroke: '#ffaa00'}});
   }
 
